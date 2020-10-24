044b25530738677db27932a00d7e3baf09db828d
josh hall <HallHammR@gmail.com>
2020-10-24 13:32:35 -0400

    asdf

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
-app.use('/api/places', placesRoutes);
+app.use(bodyParser.json());
 
-app.listen(5000);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect('mongodb+srv://larpUser:Butttown42069@cluster0.sg7os.mongodb.net/places?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,11 +1,39 @@
 const express = require('express');
+const { check } = require('express-validator');
+
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-        console.log('Get request in places');
-        res.json({message: 'It workds'});
-});
+router.get('/:pid', placesControllers.getPlaceById);
 
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
+
+router.post(
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
+);
+
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
+
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
